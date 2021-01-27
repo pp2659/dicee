0eca771a75500414733f1b5b241c927059591c25
PAYEL <pp2659@srmist.edu.in>
2021-01-27 20:02:48 +0530

    dicee

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,68 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(child: Text('Dicee')),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
+
+
       ),
     ),
   );
 }
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
 
-class DicePage extends StatelessWidget {
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber =1;
+
+  void changeDiceFace(){
+    setState(() {
+      leftDiceNumber= Random().nextInt(6)+1;
+      rightDiceNumber= Random().nextInt(6)+1;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+
+
+
+          Expanded(
+            //flex: 2,
+            child: FlatButton(
+
+              onPressed: (){
+                changeDiceFace();
+                },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+
+          Expanded(
+            //flex: 1,
+            child: FlatButton(
+
+                onPressed: (){
+                  changeDiceFace();
+                  },
+                child: Image.asset('images/dice$rightDiceNumber.png')),
+          ),
+        ],
+      ),
+    );
   }
 }
+
